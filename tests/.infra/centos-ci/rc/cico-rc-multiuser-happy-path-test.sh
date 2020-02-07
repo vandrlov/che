5c439e28cf3e5c06e1ba8e41461ad5829516a47c
Aleksandr Shmaraiev <oshmarai@redhat.com>
2020-02-07 11:40:10 +0200

    Update the patch of 'custom-resource' file

--- tests/.infra/centos-ci/rc/cico-rc-multiuser-happy-path-test.sh
+++ tests/.infra/centos-ci/rc/cico-rc-multiuser-happy-path-test.sh
@@ -14,7 +14,6 @@ function prepareCustomResourceFile() {
   echo "Patch custom-resource.yaml"
   cd /tmp
   wget https://raw.githubusercontent.com/eclipse/che-operator/master/deploy/crds/org_v1_che_cr.yaml -O custom-resource.yaml
-  sed -i "s@openShiftoAuth: false@openShiftoAuth: true@g" /tmp/custom-resource.yaml
   sed -i "s@server:@server:\n    customCheProperties:\n      CHE_LIMITS_USER_WORKSPACES_RUN_COUNT: '-1'@g" /tmp/custom-resource.yaml
   sed -i "s/customCheProperties:/customCheProperties:\n      CHE_WORKSPACE_AGENT_DEV_INACTIVE__STOP__TIMEOUT__MS: '300000'/" /tmp/custom-resource.yaml
   sed -i "s@identityProviderImage: 'quay.io/eclipse/che-keycloak:nightly'@identityProviderImage: 'quay.io/eclipse/che-keycloak:$RELEASE_TAG'@g" /tmp/custom-resource.yaml
