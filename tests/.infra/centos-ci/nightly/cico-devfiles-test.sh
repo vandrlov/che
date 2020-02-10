9f0f66daea070cfcf07b14a912c83fea4e571311
musienko maksym <mmusiien@redhat.com>
2020-02-10 21:37:30 +0200

    add script for run of devfile testsuite
    Signed-off-by: musienko maksym <mmusiien@redhat.com>
    
    add jq client
    
    revert defaults

---
+++ tests/.infra/centos-ci/nightly/cico-devfiles-test.sh
@@ -0,0 +1,15 @@
+set -x
+
+echo "========Starting nigtly test job $(date)========"
+source tests/.infra/centos-ci/functional_tests_utils.sh
+installKVM
+installStartDocker
+installOC
+installCheCtl
+installJQ
+installAndStartMinishift
+loginToOpenshiftAndSetDevRole
+deployCheIntoCluster
+createTestUserAndObtainUserToken
+runDevfileTestSuite
+archiveArtifacts "che-devfile-test"
